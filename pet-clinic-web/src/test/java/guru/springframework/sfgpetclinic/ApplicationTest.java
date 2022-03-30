ae6a708322c6efcbfa2ca3ba78c6d71e9f312b40
iSidorov88 <wooodward77@gmail.com>
2022-03-30 22:18:49 +0300

    making project multimoduled

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/ApplicationTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/ApplicationTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
-
+public class ApplicationTest {
     @Test
-    void contextLoads() {
-    }
+    public void contextLoads(){
 
+    }
 }
