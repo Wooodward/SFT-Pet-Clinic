0cabb7f846c6e34fd2e19bc5cd6bd22cd3561d21
iSidorov88 <wooodward77@gmail.com>
2022-04-08 17:40:41 +0300

    Added JPA repositories

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repository/VetRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.repository;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VetRepository extends CrudRepository<Vet, Long> {
+}
